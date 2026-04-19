82fa0aa247fc14b9ba8e2af8e9f0498460399af0
Vaibhav S <vaibhavs@vastedge.com>
2026-04-19 21:45:50 +0530

    test

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -18,7 +18,7 @@ const api = axios.create({
     'Content-Type': 'application/json',
   },
 });
-
+//test
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('swiftridex_token');
 
